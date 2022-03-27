f97143c7ac6447041b9d846e0deee10e0955cfc0
Sujeet Padhi <sujitclick7.sp@gmail.com>
2022-03-27 19:58:02 +0530

    Updated db

--- src/services/child.service.js
+++ src/services/child.service.js
@@ -1,7 +1,7 @@
 import httpClient from '../http-common';
 
 const getAll = () => {
-    return httpClient.get('/child/getAll');
+    return httpClient.get('/api/v1/children');
 }
 
 export default {getAll};
